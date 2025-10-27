8a93c258512c8614d7402180bff6cc207ddc7918
Princess-O1633 <potomacadaeze5@gmail.com>
2025-10-26 20:29:56 -0700

    Montages  and Data description complete

--- check.py
+++ check.py
@@ -3,10 +3,10 @@ import mne
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load the EEG file (use .set or .fdt + .json depending on your format)
+# Load the EEG file
 raw = mne.io.read_raw_eeglab(r'rEEG/sub-001/eeg/sub-001_task-Rest_eeg.set', preload=True)
 
-# Pick only the 60 channels used in analysis (exclude Iz, I1, I2)
+# Pick 60 channels used in analysis (exclude Iz, I1, I2)
 channels_to_use = ['Fp1', 'Fz', 'F3', 'F7', 'FT9', 'FC5', 'FC1', 'C3', 'T7', 'TP9',
                    'CP5', 'CP1', 'P3', 'P7', 'O1', 'Oz', 'O2', 'P4', 'P8', 'TP10',
                    'CP6', 'CP2', 'Cz', 'C4', 'T8', 'FT10', 'FC6', 'FC2', 'F4', 'F8',
@@ -16,7 +16,7 @@ channels_to_use = ['Fp1', 'Fz', 'F3', 'F7', 'FT9', 'FC5', 'FC1', 'C3', 'T7', 'TP
                    'AF4', 'F2', 'FCz']
 raw.pick_channels(channels_to_use)
 
-# Plot the montage (2D top view)
+# Plot the montages (2D and 3D)
 raw.plot_sensors(show_names=True, kind='topomap', title='60-channel EEG Montage')
 plt.show()
 raw.plot_sensors(kind='3d', show_names=True, title='60-channel EEG Montage (3D)')
